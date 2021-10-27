996a28d22cb4881b658724a2467bba9b24b456ca
EmiCucit <emicu144@gmail.com>
2021-10-27 18:22:03 -0300

    Contenido dinamico en el detalle

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -6,19 +6,17 @@ import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import styles from '../../styles/Home.module.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import { useContext } from 'react';
 
 
 
-export default function Products(detail) {
+export default function Products({ detail }) {
     const router = useRouter();
-
     if(router.isFallback) return <h1>Cargando... </h1>
   
     return (
     <div className={styles.container}>
       <Head>
-        <script src="https://kit.fontawesome.com/117d4d8bac.js" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/117d4d8bac.js" crossOrigin="anonymous"></script>
         <title>Ian Digital Talent App</title> 
         <link rel="icon" href="/logo.png" />
       </Head>
@@ -28,15 +26,16 @@ export default function Products(detail) {
             <div className={styles.detalleProducto}>
                 <div className={styles.displayDetalle}>
                     <div className={styles.imagenDetalle}>
-                        <Image src="/laptop.jpg" alt="Laptop" width={290} height={290}></Image>
+                        <Image src={detail.assets[0].source} alt="Laptop" width={290} height={290}></Image>
                     </div>
                     <div className={styles.displayFlex}>
                         <div className={styles.caracteristicasProducto}>
                             <div className={styles.displayNombre}>
-                                <p className={styles.nombreProducto}>Nombre</p>
-                                <p className={styles.precioProducto}>$150000</p>
+                                <p className={styles.nombreProducto}>{detail.name}</p>
+                                <p className={styles.precioProducto}>${detail.variants[0].price}</p>
                             </div>
-                            <p className={styles.categoriaProducto}>Categoria</p>
+                            <p className={styles.categoriaProducto}>{detail.collections[0].name}</p>
+                            <p className={styles.categoriaProducto}>{detail.collections[1].name}</p>
                         </div>
                         <div className={styles.displayBotones}>
                             <Link href="/cart">
@@ -61,49 +60,61 @@ export default function Products(detail) {
   )
 }
 
-export async function getStaticPaths(){
-    return{
-        paths : [{ params : { id :1 } }],
-        fallback : true
-    }
-}
+export async function getStaticPaths() {
+  const client = new ApolloClient({
+    uri:"http://localhost:3000/shop-api",
+    cache: new InMemoryCache()
+  })
 
-export async function getStaticProps() {
-    const { params } = context;
-    const { id } = params
-    console.log(id)
-    console.log(params)
-    const client = new ApolloClient({
-      uri:"http://localhost:3000/shop-api",
-      cache: new InMemoryCache()
-    })
-  
-    const {data} = await client.query({
-      query: gql`
-        query ObtenerProductos{
-          products{
-            items{
-              id,
-              assets{
-                source
-              },
-              name,
-              collections{
-                name 
-              },
-              variants{
-                price
-              },
-            }
+  const { data } = await client.query({
+    query: gql`
+      query ObtenerIds{
+        products{
+          items{
+            id
           }
         }
-      `
-    })
-    
-  
-    return{
-      props:{
-        detail: data.products.items,
-      }
+      } 
+    `
+  })
+  const paths = data.products.items.map((path) => ({
+    params: { id: path.id },
+  }))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps( paths ) {
+  const id = paths.params.id
+  const client = new ApolloClient({
+    uri:"http://localhost:3000/shop-api",
+    cache: new InMemoryCache()
+  })
+
+  const { data } = await client.query({
+    query: gql`
+      query ObtenerProducto{
+        product(id:${id}){
+          id,
+          name,
+          description,
+          collections{
+            name
+          },
+          assets{
+            source
+          },
+          variants{
+            price
+          }
+        }
+      }    
+    `
+  })
+
+  console.log(data)
+  return{
+    props:{
+      detail: data.product,
     }
   }
+} 
